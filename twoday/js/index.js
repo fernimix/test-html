0df87515b235fff79176322d5ab8286cf32e0923
Fernando López León <fernandolopezleon@gmail.com>
2016-10-18 19:10:32 +0100

    Add files via upload

--- twoday/js/index.js
+++ twoday/js/index.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
   });
 });
 
-/* */
+/* Acordeón */
+// Configuramos la librería jQuery Accordion
+$('.accordion').accordion({
+  transitionSpeed: 400,        // velocidad del acordeón
+  transitionEasing: 'ease'    // ritmo del acordeón (linear, ease-in…)
+});
+
+/* Mensaje en pantalla */
+// Configuramos la librería Ouibounce
+ouibounce(document.getElementById('modal-card'), {
+  aggressive: true,            // borra la cookie viewedOuibounceModal
+  timer: 5                        // espera (evita falsos positivos)
+});
